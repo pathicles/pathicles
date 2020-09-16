6c058391aeb08a08ce299733d8d56136cb1e64dc
Dirk Rathje <post@dirk-rathje.de>
2020-09-16 11:33:30 +0200

    feat(app): adds vue3 support

---
+++ packages/@pathicles/app/src/App.vue
@@ -0,0 +1,25 @@
+/* eslint-env browser */
+
+<template>
+  <div id="app">
+    <router-view :key="$route.fullPath"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+    printMode: function () {
+      const parsedUrl = new URL(window.location.href)
+      return parsedUrl.searchParams.get('print') !== null
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+body
+  margin 0
+  padding 0
+</style>
