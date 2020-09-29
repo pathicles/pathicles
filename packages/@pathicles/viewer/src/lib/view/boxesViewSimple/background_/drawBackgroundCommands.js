76af976c12e044b6b3a91b80145143da869ce584
Dirk Rathje <post@dirk-rathje.de>
2020-09-29 09:34:21 +0200

    feat(viewer): remove sun backgroundf

---
+++ packages/@pathicles/viewer/src/lib/view/boxesViewSimple/background_/drawBackgroundCommands.js
@@ -0,0 +1,36 @@
+import createCube from 'primitive-cube'
+import { fromTranslation } from 'gl-mat4'
+
+import frag from './background.frag'
+import vert from './background.vert'
+
+export default function (regl, { stageGrid }) {
+  // const stage = createCube(stageGrid.size, stageGrid.size * 10, stageGrid.size)
+  const stage = createCube(1, 1, 1)
+
+  // let model = fromTranslation([], [0, -stageGrid.size * 5, 0])
+  let model = fromTranslation([], [0, 0, 0])
+
+  return regl({
+    primitive: 'triangles',
+    elements: stage.cells,
+    cull: {
+      enable: true,
+      face: 'front'
+    },
+    attributes: {
+      aPosition: stage.positions,
+      uv: stage.uvs
+    },
+    uniforms: {
+      uResolution: [stageGrid.size, stageGrid.size],
+      uSunPosition: (context) => [
+        context.viewportHeight / 2,
+        (context.viewportWidth / 4) * 3
+      ],
+      model
+    },
+    vert,
+    frag
+  })
+}
