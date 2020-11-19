883f885585c2357d0addc43bef4c04e235d3af4b
Dirk Rathje <post@dirk-rathje.de>
2020-11-19 08:19:19 +0100

    feat(core): save data texture as image

--- packages/@pathicles/prerendered/tools/prerender.js
+++ packages/@pathicles/prerendered/tools/prerender.js
@@ -1,8 +1,10 @@
 /* eslint-env node, browser */
 
-const prerender = require('puppeteer')
+const prerender = require('puppeteer-core')
 const path = require('path')
 const fs = require('fs-extra-plus')
+const savePixels = require('save-pixels')
+const ndarray = require('ndarray')
 
 const sharp = require('sharp')
 
@@ -22,9 +24,9 @@ const outputFolderPath = path.join(
 )
 
 const jobs = [
-  { preset: 'story-electric' },
-  { preset: 'story-quadrupole' },
-  { preset: 'story-dipole' }
+  { preset: 'story-electric' }
+  // { preset: 'story-quadrupole' },
+  // { preset: 'story-dipole' }
 ]
 
 const queryString = '&debug=false&print=true&prerender=true'
@@ -32,9 +34,9 @@ const queryString = '&debug=false&print=true&prerender=true'
 // eslint-disable-next-line no-unused-vars
 const createImages = async () => {
   const browser = await prerender.launch({
-    headless: false
-    // executablePath:
-    //   '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    headless: false,
+    executablePath:
+      '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
   })
 
   for (let i = 0; i < jobs.length; i++) {
@@ -59,6 +61,54 @@ const createImages = async () => {
     const dump = await page.evaluate(() => {
       return window.pathicles.simulation.log(false)
     })
+
+    // create a write stream for the file
+    let output = fs.createWriteStream(
+      path.join(outputFolderPath, preset + '.png')
+    )
+
+    const particleCount = dump.configuration.model.emitter.particleCount
+    const bufferLength = dump.configuration.model.bufferLength
+
+    const shape = [particleCount, bufferLength]
+
+    console.log(shape)
+    console.log(dump.position)
+    console.log(ndarray(new Uint8Array(dump.position), shape))
+
+    savePixels(ndarray(dump.position, shape), 'png').pipe(output)
+
+    // // // create a PNG encoder stream
+    // let encoder = new PNGEncoder(shape[0], shape[1], {
+    //   colorSpace: 'rgba'
+    // })
+    //
+    // // create a readPixels stream for the FBO
+    // // flip image on Y axis due to FBO coordinates
+    // let pixels = pixelStream(
+    //   dump.regl._gl,
+    //   dump.positionFBO._framebuffer,
+    //   shape,
+    //   {
+    //     flipY: true
+    //   }
+    // )
+    // //
+    // // // send pixels to encoder, then to file
+    // pixels.pipe(encoder)
+    // encoder.pipe(output)
+    //
+    // // // when file writing is finished
+    // output.on('close', function () {
+    //   console.log('Saved %dx%d buffer to image.png', shape[0], shape[1])
+    //   console.timeEnd('render')
+    //
+    //   // quit the dev tools window when not in dev mode
+    //   if (process.env.NODE_ENV === 'production') {
+    //     window.close()
+    //   }
+    // })
+
     fs.writeJSONSync(path.join(outputFolderPath, preset + '.json'), {
       iteration: dump.iteration,
       configuration: dump.configuration,
@@ -76,7 +126,7 @@ const imagePaths = async () => {
 }
 
 const convertImagesSharp = async () => {
-  const qualities = [20, 40, 60, 80]
+  const qualities = [50] //[20, 40, 60, 80]
 
   await Promise.all(
     (await imagePaths()).map(async (imgPath) => {
