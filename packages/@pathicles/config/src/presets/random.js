9495a9e3663b445d85e6b6a7b3121f8f9cec7672
Dirk Rathje <post@dirk-rathje.de>
2021-01-14 09:31:59 +0100

    build: removed circular dependency

--- packages/@pathicles/config/src/presets/random.js
+++ packages/@pathicles/config/src/presets/random.js
@@ -1,5 +1,10 @@
 import { RUNNER_MODE } from '../constants'
-import { boundedRandom } from '@pathicles/core/src/lib/utils/random'
+let seed = 1
+export const random = () => {
+  let x = Math.sin(seed++) * 10000
+  return x - Math.floor(x)
+}
+export const boundedRandom = (min = -1, max = 1) => random() * (max - min) + min
 
 export default {
   name: 'random',
